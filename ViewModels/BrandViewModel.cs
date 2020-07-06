291d57e937296e7f0c3298de29ea86575fa0399e
AlfMatute <alf.matute@gmail.com>
2020-07-06 13:50:34 -0600

    View Models added

---
+++ ViewModels/BrandViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionTrackerMVC.ViewModels
+{
+    public class BrandViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BrandId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string BrandName { get; set; }
+
+        [MaxLength(250)]
+        public string Description { get; set; }
+    }
+}
---
+++ ViewModels/CategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionTrackerMVC.ViewModels
+{
+    public class CategoryViewModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+        [Required]
+        public bool Active { get; set; }
+    }
+}
